73a059d8231de94da2b8dfb30b68dbc8f6da5cfd
md-sazzadul <md.sazzadul.islam15@gmail.com>
2026-02-20 12:32:19 +0100

    make api call to load products by category

--- products.js
+++ products.js
@@ -6,6 +6,17 @@ const loadCategories = () => {
     .catch((error) => console.error("Error fetching categories:", error));
 };
 
+// load products by category
+const loadProductsByCategory = (category) => {
+  const url = `https://fakestoreapi.com/products/category/${category}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) =>
+      console.error("Error fetching products by category:", error),
+    );
+};
+
 const displayCategories = (categories) => {
   const categoriesContainer = document.getElementById("categories-container");
 
@@ -13,7 +24,7 @@ const displayCategories = (categories) => {
     const categoryButton = document.createElement("button");
 
     categoryButton.innerHTML = `
-        <button class="btn btn-sm bg-indigo-600 hover:bg-indigo-700 text-white border-none px-6">
+        <button onclick="loadProductsByCategory('${category}')" class="btn btn-sm bg-indigo-600 hover:bg-indigo-700 text-white border-none px-6">
             ${category}
         </button>`;
 
